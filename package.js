d4c177fc3e32863cd58c8decd7004f0f5e4096ca
Nick Wientge <nick@exygeninteractive.com>
2014-12-14 13:49:15 -0800

    initial commit

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  name: 'meteoric:ionic-sass',
+  summary: 'Ionic SASS files bundled for Meteor.',
+  version: '0.1.0',
+  git: 'https://github.com/meteoric/ionic-sass'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.use('stevezhu:sass@1.3.2');
+  api.addFiles('sass_include_paths.json', 'server');
+});
