75203f461f5358aed3e8f6c6adde1304419636f7
Nick Wientge <nick@exygeninteractive.com>
2015-02-15 18:42:37 -0800

    upgrade fourseven:scss depenendcy, publish new version

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'meteoric:ionic-sass',
   summary: "Ionic's CSS Framework in SASS and bundled for Meteor.",
-  version: '0.1.8',
+  version: '0.1.9',
   git: 'https://github.com/meteoric/ionic-sass'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use("fourseven:scss@1.0.0", ["server"]);
+  api.use("fourseven:scss@2.0.0", ["server"]);
   api.imply("fourseven:scss", ["server"]);
 
   api.addFiles([
